1cb1fa9575efa61d63aa6218946d971fba6082d8
Nicolás G. Arbio <nicolasarbio@outlook.com>
2025-06-16 23:48:49 -0300

    Initialize sales-service

---
+++ sales-service/src/test/java/com/nicoarbio/cardealership/sales_service/SalesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicoarbio.cardealership.sales_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
