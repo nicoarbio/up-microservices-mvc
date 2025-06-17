8edaa5fec985859290a0efab7950d149fb5fd0d6
Nicolás G. Arbio <nicolasarbio@outlook.com>
2025-06-17 00:32:53 -0300

    maintenance-service: Update application.yml + @EnableFeignClients

--- maintenance-service/src/main/java/com/nicoarbio/cardealership/maintenance_service/MaintenanceServiceApplication.java
+++ maintenance-service/src/main/java/com/nicoarbio/cardealership/maintenance_service/MaintenanceServiceApplication.java
@@ -2,7 +2,9 @@ package com.nicoarbio.cardealership.maintenance_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class MaintenanceServiceApplication {
 
