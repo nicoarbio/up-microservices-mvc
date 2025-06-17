282e9599aeb918e0550efb16b2a4624a4522ed27
Nicolás G. Arbio <nicolasarbio@outlook.com>
2025-06-17 00:32:59 -0300

    sales-service: Update application.yml + @EnableFeignClients

--- sales-service/src/main/java/com/nicoarbio/cardealership/sales_service/SalesServiceApplication.java
+++ sales-service/src/main/java/com/nicoarbio/cardealership/sales_service/SalesServiceApplication.java
@@ -2,7 +2,9 @@ package com.nicoarbio.cardealership.sales_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class SalesServiceApplication {
 
