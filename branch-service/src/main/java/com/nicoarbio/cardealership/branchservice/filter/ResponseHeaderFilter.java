2484aee8127ad5d54bdcd376144de5dc9ba53319
Nicolás G. Arbio <nicolasarbio@outlook.com>
2025-06-17 15:47:35 -0300

    branch-service: propagate only X-Service-Name

--- branch-service/src/main/java/com/nicoarbio/cardealership/branchservice/filter/ResponseHeaderFilter.java
+++ branch-service/src/main/java/com/nicoarbio/cardealership/branchservice/filter/ResponseHeaderFilter.java
@@ -8,13 +8,11 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.util.UUID;
 
 @Component
 public class ResponseHeaderFilter extends OncePerRequestFilter {
 
     private final static String X_SERVICE_NAME = "X-Service-Name";
-    private final static String X_REQUEST_ID = "X-Request-ID";
 
     @Value("${spring.application.name}")
     private String SERVICE_NAME;
@@ -22,13 +20,7 @@ public class ResponseHeaderFilter extends OncePerRequestFilter {
     @Override
     public void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
 
-        String requestId = request.getHeader(X_REQUEST_ID);
-        if (requestId == null || requestId.isBlank()) {
-            requestId = UUID.randomUUID().toString(); // Using Api Gateway, we should not generate a new requestId here, we expect it to be set by the gateway
-        }
-
         response.setHeader(X_SERVICE_NAME, SERVICE_NAME);
-        response.setHeader(X_REQUEST_ID, requestId); // This requestId should be also configured in the context/MDC
 
         chain.doFilter(request, response);
     }
