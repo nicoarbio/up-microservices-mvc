33701dab7e5a5522dd0e4db69cae49169dcbe40a
Nicolás G. Arbio <nicolasarbio@outlook.com>
2025-06-18 01:56:30 -0300

    sales-service: add @EnableDiscoveryClient

--- sales-service/src/main/java/com/nicoarbio/cardealership/sales_service/SalesServiceApplication.java
+++ sales-service/src/main/java/com/nicoarbio/cardealership/sales_service/SalesServiceApplication.java
@@ -2,9 +2,11 @@ package com.nicoarbio.cardealership.sales_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
+@EnableDiscoveryClient
 @SpringBootApplication
 public class SalesServiceApplication {
 
